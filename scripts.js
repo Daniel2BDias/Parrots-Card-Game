1d8ab8f340ecf4791be617d030c8d64b5657e3fe
Daniel2BDias <118937135+Daniel2BDias@users.noreply.github.com>
2023-01-16 16:42:11 -0300

    inicia o jogo e monta o board

--- scripts.js
+++ scripts.js
@@ -1,1 +1,37 @@
-const qtdCartas = prompt("quantas cartas é o jogo (4-14, somente pares)");
+const board = document.querySelector('.jogo');
+
+const parrots = [
+
+];
+
+const timer = document.getElementById("timer");
+
+const jogadas = document.getElementById("jogadas");
+
+let qtdCartas = prompt("quantas cartas? 4-14, somente pares");
+while (qtdCartas < 4 || qtdCartas > 14 || qtdCartas % 2 !== 0){
+    prompt("quantas cartas? 4-14, somente pares");
+    qtdCartas = prompt("quantas cartas? 4-14, somente pares");
+}
+
+
+for(let i = 0; i < qtdCartas; i++){
+board.innerHTML += `<div class="carta" onclick="clicked(this)">
+<div class="face">
+  <img src="Gifs/front.png">
+</div>
+<div class="back-face face">
+  <img src="Gifs/${}.gif">
+</div>
+</div>`;
+}
+
+function clicked(clicada){
+
+    const first = `${clicada}.firstChild`;
+    const last = `${clicada}.lastChild`;
+    const front = document.querySelector(first);
+    const back = document.querySelector(last);
+    front.classList.add('front-face');
+    back.classList.remove('back-face');
+}
