bc8bda89c81a1586db924f040f3f9e346b4b031a
Daniel Dias <daniiel2dias@gmail.com>
2023-11-15 07:33:27 -0300

    visual effects adjustments and plays counter adjustmented

--- scripts.js
+++ scripts.js
@@ -1,47 +1,45 @@
-const board = document.querySelector('.jogo');
+const board = document.querySelector(".jogo");
 
 const parrots = [
-    'parrot1',
-    'parrot2',
-    'parrot3',
-    'parrot4',
-    'parrot5',
-    'parrot6',
-    'parrot7'
+  "parrot1",
+  "parrot2",
+  "parrot3",
+  "parrot4",
+  "parrot5",
+  "parrot6",
+  "parrot7",
 ];
 
 const jogadas = document.getElementById("jogadas");
 
 let qtdCartas = prompt("quantos pares? 2-7");
 
-while (qtdCartas < 2 || qtdCartas > 7){
-   prompt("quantos pares? 2-7")
-    qtdCartas = prompt("quantos pares? 2-7");
+while (qtdCartas < 2 || qtdCartas > 7) {
+  prompt("quantos pares? 2-7");
+  qtdCartas = prompt("quantos pares? 2-7");
 }
 
 const timer = document.getElementById("timer");
 let timerCounter = 0;
 let interval = setInterval(contador, 1000);
 
-function contador(){
+function contador() {
   timerCounter++;
   timer.innerHTML = timerCounter;
 }
 
+const divided = qtdCartas;
 
- const divided = qtdCartas;
+const slicedParrots = parrots.slice(0, divided);
 
- const slicedParrots = parrots.slice(0, divided);
+const slicedParrotsClone = [...slicedParrots];
 
- const slicedParrotsClone = [...slicedParrots];
+const parrotsFusion = slicedParrots.concat(slicedParrotsClone);
 
- const parrotsFusion = slicedParrots.concat(slicedParrotsClone);
+const shuffled = parrotsFusion.sort(() => Math.random() - 0.5);
 
- const shuffled = parrotsFusion.sort(() => Math.random() - 0.5);
-
-for(let i = 0; i < qtdCartas*2; i++){
-
-   board.innerHTML += `<div data-test="card" data-parrot="${shuffled[i]}" class="carta" onClick="viraCarta(this)">
+for (let i = 0; i < qtdCartas * 2; i++) {
+  board.innerHTML += `<div data-test="card" data-parrot="${shuffled[i]}" class="carta" onClick="viraCarta(this)">
     <div class="front-face face">
       <img data-test="face-down-image" src="Gifs/front.png">
     </div>
@@ -49,86 +47,73 @@ for(let i = 0; i < qtdCartas*2; i++){
       <img data-test="face-up-image" src="Gifs/${shuffled[i]}.gif">
     </div>
     </div>`;
-    }
+}
 
-    let primeira = "";
-    let segunda = "";
+let primeira = "";
+let segunda = "";
 
-    function verificaCarta(){
+function verificaCarta() {
+  const verPrimeira = primeira.getAttribute("data-parrot");
+  const verSegunda = segunda.getAttribute("data-parrot");
 
-      const verPrimeira = primeira.getAttribute('data-parrot');
-      const verSegunda = segunda.getAttribute('data-parrot');
+  const jogadasHtml = jogadas.innerHTML;
+  const numerico = +jogadasHtml;
+  jogadas.innerHTML = numerico + 1;
 
-      if(verPrimeira === verSegunda){
+  if (verPrimeira === verSegunda) {
+    setTimeout(() => {
+      primeira.classList.add("acertou");
+      segunda.classList.add("acertou");
 
-        setTimeout(() => {
+      primeira = "";
+      segunda = "";
+    }, 500);
+  } else {
+    setTimeout(() => {
+      primeira.classList.remove("vira-carta");
+      segunda.classList.remove("vira-carta");
 
-          primeira.classList.add('acertou');
-         segunda.classList.add('acertou');
-        
-         primeira = "";
-        segunda = "";
-
-        }, 500);
-
-      
-      } else {
-
-        setTimeout(() => {
-        
-        primeira.classList.remove('vira-carta');
-        segunda.classList.remove('vira-carta');
-        
-        primeira = "";
-        segunda = "";
-        
-      }, 1000);
-      
-      }
-
-    }
-    function viraCarta(clicada){
-
-      if(clicada.classList.contains('vira-carta')){
-        return;
-      }
-     if(primeira !== "" && segunda !== ""){
-            return;
-        }
-
-      clicada.classList.add('vira-carta');
-
-      if(primeira === ""){
-      primeira = clicada;
-      } else if (segunda === ""){
-        segunda = clicada;
-      }
-
-      const jogadasHtml = jogadas.innerHTML;
-
-      const numerico = +jogadasHtml
-
-      jogadas.innerHTML = numerico+1;
-
-      verificaCarta();
-      fimDoJogo();
-    }
-      let fim = +qtdCartas*2;
-    
-    function fimDoJogo(){
-      
-
-      setTimeout(() => {if (document.querySelectorAll('.vira-carta').length === fim){ 
-
-        clearInterval(interval);
-
-       alert(`Você ganhou em ${jogadas.innerHTML} jogadas! A duração do jogo foi de ${timer.innerHTML} segundos!`);
-
-    let reinicio = confirm("deseja recomeçar?");
-
-    if(reinicio){
-      location.reload();
-    };
+      primeira = "";
+      segunda = "";
+    }, 1000);
   }
-}, 1000);
+}
+
+function viraCarta(clicada) {
+  if (clicada.classList.contains("vira-carta")) {
+    return;
+  }
+  if (primeira !== "" && segunda !== "") {
+    return;
+  }
+
+  clicada.classList.add("vira-carta");
+
+  if (primeira === "") {
+    primeira = clicada;
+  } else if (segunda === "") {
+    segunda = clicada;
+  }
+
+  verificaCarta();
+  fimDoJogo();
+}
+let fim = +qtdCartas * 2;
+
+function fimDoJogo() {
+  setTimeout(() => {
+    if (document.querySelectorAll(".vira-carta").length === fim) {
+      clearInterval(interval);
+
+      alert(
+        `Você ganhou em ${jogadas.innerHTML} jogadas! A duração do jogo foi de ${timer.innerHTML} segundos!`
+      );
+
+      let reinicio = confirm("deseja recomeçar?");
+
+      if (reinicio) {
+        location.reload();
+      }
+    }
+  }, 1000);
 }
